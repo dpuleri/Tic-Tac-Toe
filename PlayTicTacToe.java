4a533a9e1aa4e5351af8e580602e7c4b436e7436
dpuleri <daniel.puleri@gmail.com>
2014-02-22 17:08:24 -0500

    Tic-Tac-Toe driver

---
+++ PlayTicTacToe.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+public class PlayTicTacToe {
+    public static void main(String[] args) {
+    //human is O and Computer is X. always.
+        LinkedList<int[]> moves = new LinkedList<int[]>();
+        TicBoard board = new TicBoard();
+        ComputerPlayer player = new ComputerPlayer(board);
+        Scanner sc = new Scanner(System.in);
+        while (!board.checkWin() && (moves.toArray().length < 9)) {
+            System.out.println(board);
+            int humanMove = Integer.parseInt(sc.nextLine());
+            board.setTile("O", humanMove);
+            moves.addLast(new int[]{humanMove / 3, humanMove % 3});
+            player.updateSet(moves.getLast());
+            if (!board.checkWin() && (moves.toArray().length < 9)) {
+                int[] computerMove = player.getNextMove();
+                board.setTile("X", computerMove);
+                moves.addLast(computerMove);
+                player.updateSet(moves.getLast());
+            }
+        }
+        System.out.println(board);
+    }
+}
